3034cbdf605c9d4f0bb05d62d735cdeb337d05b4
shubham mali <98689609+malishubham025@users.noreply.github.com>
2024-08-29 22:26:10 +0530

    Delete public/js directory

--- public/js/front.js
+++
@@ -1,9 +0,0 @@
-// alert("hi");
-const socket = io(); 
-let input=document.querySelector(".input");
-function handleJoin(){
-    socket.emit("join-room",input,socket.id);
-}
-function handleStart(){
-    socket.emit("start",input);
-}
